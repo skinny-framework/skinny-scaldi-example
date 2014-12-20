c6858f474a352cf4c12c8212430b9107e960e3cf
Kazuhiro Sera <seratch@gmail.com>
2014-12-20 19:44:22 +0900

    Fix tests

--- src/test/scala/model/AWSClientImplSpec.scala
+++ src/test/scala/model/AWSClientImplSpec.scala
@@ -5,6 +5,8 @@ class AWSClientImplSpec extends FunSpec with Matchers {
 
   describe("AWSClientImpl") {
     it("should be created") {
+      System.setProperty("aws.accessKeyId", "aaa")
+      System.setProperty("aws.secretKey", "bbb")
       val aws = new AWSClientImpl
       aws should not equal (null)
     }

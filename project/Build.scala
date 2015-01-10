397119990e0102527f2acbc7cac8697033f31886
Kazuhiro Sera <seratch@gmail.com>
2015-01-10 12:00:05 +0900

    Scala 2.11.5

--- project/Build.scala
+++ project/Build.scala
@@ -20,8 +20,8 @@ object SkinnyAppBuild extends Build {
   val appVersion = "0.1.0-SNAPSHOT"
 
   val skinnyVersion = "1.3.+"
-  val scalatraVersion = "2.3.0"
-  val theScalaVersion = "2.11.4" 
+  val scalatraVersion = "2.3.+"
+  val theScalaVersion = "2.11.5"
   val jettyVersion = "9.2.6.v20141205"
 
   lazy val baseSettings = ScalatraPlugin.scalatraWithJRebel ++ herokuSettings ++ Seq(
@@ -39,11 +39,11 @@ object SkinnyAppBuild extends Build {
       "org.skinny-framework"    %% "skinny-assets"       % skinnyVersion,
       "org.skinny-framework"    %% "skinny-task"         % skinnyVersion,
       "org.skinny-framework"    %% "skinny-scaldi"       % skinnyVersion,
-      "com.github.seratch"      %% "awscala"             % "0.4.2",
-      "org.skinny-framework"    %  "skinny-logback"      % "1.0.3",
-      "org.apache.commons"      %  "commons-dbcp2"       % "2.0.1",
-      "com.h2database"          %  "h2"                  % "1.4.184",      // your own JDBC driver
-      "ch.qos.logback"          %  "logback-classic"     % "1.1.2",
+      "com.github.seratch"      %% "awscala"             % "0.4.+",
+      "org.skinny-framework"    %  "skinny-logback"      % "1.0.+",
+      "org.apache.commons"      %  "commons-dbcp2"       % "2.0.+",
+      "com.h2database"          %  "h2"                  % "1.4.+",      // your own JDBC driver
+      "ch.qos.logback"          %  "logback-classic"     % "1.1.+",
       "org.skinny-framework"    %% "skinny-factory-girl" % skinnyVersion        % "test",
       "org.skinny-framework"    %% "skinny-test"         % skinnyVersion        % "test",
       "org.scalatra"            %% "scalatra-scalatest"  % scalatraVersion      % "test",
